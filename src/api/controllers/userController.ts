3ecf9ca68dffa638f8d2d4be92d5f49fc09222fd
Michel Mazeto <michel.mazeto.pb@compasso.com.br>
2023-04-09 21:53:05 -0400

    feat: added function list all users and get by id

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -64,3 +64,54 @@ export const deleteUser = async (req: Request, res: Response): Promise<void> => 
     res.status(500).json({ error: 'Error deleting user.' });
   }
 };
+
+export const getUserById = async (req: Request, res: Response): Promise<void> => {
+  const userId = req.params.id;
+
+  try {
+    if (!isValidObjectId(userId)) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    const user: IUser | null = await User.findById(userId);
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    APIError.handleErrorResponse(res, err);
+  }
+};
+
+export const listAllUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const queryParams: any = req.query;
+    const limit: number = parseInt(queryParams.limit) || 100;
+    const offset: number = parseInt(queryParams.offset) || 1;
+
+    const users: IUser[] = await User.find(queryParams)
+      .skip(offset - 1)
+      .limit(limit);
+
+    const totalUsers: number = await User.countDocuments(queryParams);
+
+    if (users.length === 0) {
+      res.status(404).json({ message: 'No users found.' });
+    } else {
+      res.status(200).json({
+        data: {
+          total: totalUsers,
+          limit: limit,
+          offset: offset,
+          users: users,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Error listing users.' });
+  }
+};
--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-import { createUser, updateUser, deleteUser } from '../controllers/userController';
+import { createUser, updateUser, deleteUser, getUserById, listAllUsers } from '../controllers/userController';
 import authController from '../controllers/authController';
 
 const userRouter = express.Router();
 
 userRouter.route('/')
+  .get(listAllUsers)
   .post(createUser);
 
 userRouter.route('/:id')
   .patch(updateUser)
-  .delete(deleteUser);
+  .delete(deleteUser)
+  .get(getUserById);
 
 userRouter.route('/authenticate')
   .post(authController.authenticate);
